7b4b52e35c2c75c4811d4ad2e7919ab47d324e7c
technologieinformation10-cyber <technologieinformation10@gmail.com>
2026-08-07 08:11:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * ملاحظة: التسجيلات الصوتية نفسها لا تُخزَّن هنا، بل في IndexedDB (db.js).
  */
 
-const CACHE_NAME = "quran-review-cache-v15";
+const CACHE_NAME = "quran-review-cache-v17";
 const APP_SHELL = [
   "./",
   "./index.html",
